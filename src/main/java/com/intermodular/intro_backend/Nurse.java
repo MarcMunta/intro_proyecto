7cda82c103717b950c4fe181cc9c480529b31df9
Marc Muntané <marcmclara@gmail.com>
2025-10-23 16:23:45 +0200

    Revert "Hibernate_Local"

--- src/main/java/com/intermodular/intro_backend/Nurse.java
+++ src/main/java/com/intermodular/intro_backend/Nurse.java
@@ -1,84 +1,63 @@
 package com.intermodular.intro_backend;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
-
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
-
-@Entity
-@Table(name = "enfermeros")
 public class Nurse {
-
-    @Id
-    @Column(name = "nurse_id")
-    @JsonProperty("nurse_id")
-    private Integer id;
-
-    @Column(name = "first_name", nullable = false, length = 100)
-    @JsonProperty("first_name")
-    private String firstName;
-
-    @Column(name = "last_name", nullable = false, length = 100)
-    @JsonProperty("last_name")
+    private int id;
+    private String name;
     private String lastName;
-
-    @Column(name = "email", nullable = false, unique = true, length = 150)
     private String email;
-
-    @Column(name = "password", nullable = false, length = 500)
     private String password;
 
-    protected Nurse() {
-        // JPA requirement
-    }
-
-    public Nurse(Integer id, String firstName, String lastName, String email, String password) {
+    public Nurse(int id, String name, String lastName, String email, String password) {
         this.id = id;
-        this.firstName = firstName;
+        this.name = name;
         this.lastName = lastName;
         this.email = email;
         this.password = password;
     }
 
-    public Integer getId() {
+    public int getId() {
         return id;
     }
 
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
+    public String getName() {
+        return name;
     }
 
     public String getLastName() {
         return lastName;
     }
 
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
     public String getEmail() {
         return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
     }
 
     public String getPassword() {
         return password;
     }
 
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
     public void setPassword(String password) {
         this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return "Nurse [(" + id + ") - name=" + name + ", lastName=" + lastName + ", email=" + email + ", password="
+                + password + "]";
     }
 }
